e19150d3eb5f7b8b0fccbd43dd6b945466106007
Sreeraj <sreerajs728@gmail.com>
2023-10-26 12:43:50 +0530

    sample file added

--- movie.py
+++ movie.py
@@ -2,28 +2,28 @@ import openai
 import moviepy.editor as mp
 
 # Set your OpenAI API key
-#openai.api_key = 'your_api_key'
+openai.api_key = 'your_api_key'
 
 # Example input text
 input_text = "Create a video that explains the process of photosynthesis."
 
 # Use GPT-3 to generate video description
-#response = openai.Completion.create(
- #   engine="davinci",
-  #  prompt=f"Create a video that explains the process of photosynthesis: {input_text}",
-   # max_tokens=100,
-#)
+# response = openai.Completion.create(
+#    engine="davinci",
+#    prompt=f"Create a video that explains the process of photosynthesis: {input_text}",
+#    max_tokens=100,
+# )
 
 #video_description = response.choices[0].text
 video_description = input_text
 
 # Placeholder: Use your own video clips or images
 # You should replace these with actual video content
-video_clip = mp.VideoFileClip('input_video.mp4')
+video_clip = mp.VideoFileClip('sample.mp4')
 edited_clip = video_clip.subclip(0, 10)  # Trim the clip to the first 10 seconds
 
 # Export the edited clip as a new video
-edited_clip.write_videofile('output_video.mp4', codec='libx264')
+edited_clip.write_videofile('output/output_video.mp4', codec='libx264')
 
 print("Video description:", video_description)
 print("Video created: output_video.mp4")
